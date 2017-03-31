4018e0e5a9bdae110c35872a07ddcf0a76bac42a
zhoushengzheng <zhoushengzheng@readtv.cn>
2017-03-31 16:53:19 +0800

    fix php container directory not exist

--- php/init.sh
+++ php/init.sh
@@ -3,5 +3,6 @@
 mkdir -p /data/www/wwwroot 
 cd /data/www/wwwroot
 /data/server/php/sbin/php-fpm
+mkdir -p /data/server/php/var/log/
 touch /data/server/php/var/log/php-fpm.log
 tail -f /data/server/php/var/log/php-fpm.log
